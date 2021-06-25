5f62e9618c1df4e2f1d7c096d2067063b89a61d8
Silas Jimmy <jimmysilas17@gmail.com>
2021-06-25 15:56:26 +0300

    refactor: Finish implementing article anlyzer function

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,13 +1,35 @@
 function handleSubmit(e) {
     e.preventDefault();
 
+    // Function variables
+    const polarity = {
+      "P+": "Strong positive",
+      "P": "Positive",
+      "NEU": "Neutral",
+      "N": "Negative",
+      "N+": "Strong negative",
+      "NONE": "Text has no polarity"
+    }
+
+    const subject = {
+      "OBJECTIVE": "The text does not have any subjectivity marks",
+      "SUBJECTIVE": "The text has subjective marks"
+    }
+
+    const irony = {
+      "NONIRONIC": "The text does not contain any ironic marks",
+      "IRONIC": "The text contains ironic marks"
+    }
+
+    const resultsContainer = document.querySelector("#results");
+
     // Get the text that was put into the form field
     let formText = document.getElementById('name').value
 
     // Client.checkForName(formText)
 
     // Fetch the api key
-    fetch('http://127.0.0.1:8080/getKey')
+    fetch('http://localhost:8080/getKey')
     .then(response => {
       let data = response.json();
       return data
@@ -15,19 +37,25 @@ function handleSubmit(e) {
     .then(apiDetails => {
       return Client.evaluateArticle(apiDetails.key, formText)
     })
-    .then(evaluation => {
-      // let evaluationData = evaluation.json();
-      console.log(evaluation);
+    .then(data => {
+      resultsContainer.querySelector("#form-text").innerHTML = formText;
+
+      resultsContainer.querySelector("#message").innerHTML =
+        data.status.code === "200" ? data.status.msg : "Analysis successfull!";
+
+      resultsContainer.querySelector("#message").style.color =
+        data.status.code === "200" ? "red" : "green";
+
+      resultsContainer.querySelector("#score-tag").innerHTML =
+        data.status.code === "200" ? "" : polarity[data.score_tag];
+
+      resultsContainer.querySelector("#subjectivity").innerHTML =
+        data.status.code === "200" ? "" : subject[data.subjectivity];
+
+      resultsContainer.querySelector("#irony").innerHTML =
+        data.status.code === "200" ? "" : irony[data.irony];
     })
     .catch(error => console.log(error))
-
-    // console.log("::: Form Submitted :::")
-    // fetch('http://127.0.0.1:8080/getKey')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //   let resultsContainer = document.getElementById('results');
-    //   resultsContainer.innerHTML = res.key;
-    // })
 }
 
 export { handleSubmit }
